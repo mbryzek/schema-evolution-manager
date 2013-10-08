fefbf2f5389c872aa6985767585cb57aa4ebd8c6
Michael Bryzek <michael@gilt.com>
2013-10-07 21:41:41 -0400

    Fix regexp in release script to parse Readme.md file for version tags

--- util/create-release.rb
+++ util/create-release.rb
@@ -11,7 +11,7 @@ load File.join(File.dirname(__FILE__), '../lib/all.rb')
 Library.set_verbose(true)
 
 dirty_files = Library.system_or_error("git status --porcelain").strip
-Preconditions.check_state(dirty_files == "", "Local checkout is dirty:\n%s" % [dirty_files])
+Preconditions.check_state(dirty_files == "", "Local checkout is dirty:\n%s" % dirty_files)
 
 puts "Running Tests"
 Dir.chdir("test") do
@@ -20,7 +20,7 @@ end
 puts "All tests passed"
 
 version = Version.read
-puts "Current version is %s" % [version.to_version_string]
+puts "Current version is %s" % version.to_version_string
 tmp = Ask.for_string("New version:", :default => version.next_micro.to_version_string)
 new_version = Version.new(tmp)
 
@@ -33,9 +33,9 @@ end
 new_readme = ""
 found = false
 IO.readlines("README.md").each do |l|
-  if l.match(/git checkout \d\.\d\.\d$/)
+  if l.match(/git checkout \d+\.\d+\.\d+$/)
     found = true
-    l.sub!(/git checkout \d\.\d\.\d$/, "git checkout %s" %[new_version.to_version_string])
+    l.sub!(/git checkout \d+\.\d+\.\d+$/, "git checkout %s" % new_version.to_version_string)
   end
   new_readme << l
 end
@@ -50,11 +50,11 @@ File.open("README.md", "w") { |out| out << new_readme }
 puts "Writing new_version[%s] to %s" % [new_version.to_version_string, Version::VERSION_FILE]
 Version.write(new_version)
 
-Library.system_or_error("git commit -m 'Update version to %s' VERSION README.md" % [new_version.to_version_string])
+Library.system_or_error("git commit -m 'Update version to %s' VERSION README.md" % new_version.to_version_string)
 
 puts "Creating git tag[%s]" % new_version.to_version_string
 Library.system_or_error("git tag -a -m '%s' %s" % [new_version.to_version_string, new_version.to_version_string])
 
-puts "Release tag[%s] created. Need to:" % [new_version.to_version_string]
+puts "Release tag[%s] created. Need to:" % new_version.to_version_string
 puts "  git push origin"
 puts "  git push --tags origin"
