431af4fe7f0d5acc617b0fcd54c9c7c08acb90c2
Michael Bryzek <michael@gilt.com>
2014-10-26 11:01:37 -0400

    Make attribute sort generic. transaction attribute to accept 'single' or 'none'

--- lib/schema-evolution-manager/migration_file.rb
+++ lib/schema-evolution-manager/migration_file.rb
@@ -6,14 +6,36 @@ module SchemaEvolutionManager
 
     class Attribute
 
-      attr_reader :name
+      attr_reader :name, :valid_values
 
-      def initialize(name)
+      def initialize(name, valid_values)
         @name = Preconditions.check_not_blank(name, "name cannot be blank")
+        @valid_values = Preconditions.assert_class(valid_values, Array)
       end
 
-      unless defined?(IN_TRANSACTION)
-        IN_TRANSACTION = Attribute.new("Run migration file in a single transaction")
+      unless defined?(ATTRIBUTES)
+        ATTRIBUTES = [Attribute.new("transaction", ["single", "none"])]
+      end
+
+    end
+
+    class AttributeValue
+
+      attr_reader :attribute, :value
+
+      def initialize(attribute_name, value)
+        Preconditions.assert_class(attribute_name, String)
+        @value = Preconditions.assert_class(value, String)
+
+        @attribute = Attribute::ATTRIBUTES.find { |a| a.name == attribute_name }
+        Preconditions.check_not_null(@attribute,
+                                     "Attribute with name[%s] not found. Must be one of: %s" %
+                                     [attribute_name, 
+                                      Attribute::ATTRIBUTES.map(&:name).join(" ")])
+
+        Preconditions.check_state(@attribute.valid_values.include?(@value),
+                                  "Attribute[%s] - Invalid value[%s]. Must be one of: %s" %
+                                  [@attribute.name, @value, @attribute.valid_values.join(" ")])
       end
 
     end
@@ -25,11 +47,13 @@ module SchemaEvolutionManager
       Preconditions.check_state(File.exists?(@path), "File[#{@path}] does not exist")
     end
 
-    # Returns a list of attributes from the file itself. Attributes
-    # are defined in comments at the top of the file.
+    # Returns a list of AttributeValues from the file
+    # itself. AttributeValues are defined in comments at the top of
+    # the file.
     # @see Attribute
-    def attributes
-      [Attribute::IN_TRANSACTION]
+    # @see AttributeValue
+    def attribute_values
+      [AttributeValue.new("transaction", "single")]
     end
 
   end
--- test/specs/lib/migration_file_spec.rb
+++ test/specs/lib/migration_file_spec.rb
@@ -15,6 +15,38 @@ describe SchemaEvolutionManager::MigrationFile do
     end
   end
 
+  describe "AttributeValue" do
+
+    it "accepts single" do
+      av = SchemaEvolutionManager::MigrationFile::AttributeValue.new("transaction", "single")
+      av.attribute.name.should == "transaction"
+      av.value.should == "single"
+    end
+
+    it "accepts none" do
+      av = SchemaEvolutionManager::MigrationFile::AttributeValue.new("transaction", "none")
+      av.attribute.name.should == "transaction"
+      av.value.should == "none"
+    end
+
+    it "raises error for invalid attribute name" do
+      ["", "multiple", "foo"].each do |name|
+        lambda {
+          SchemaEvolutionManager::MigrationFile::AttributeValue.new(name, "value")
+        }.should raise_error(RuntimeError, "Attribute with name[#{name}] not found. Must be one of: transaction")
+      end
+    end
+
+    it "raises error for invalid values" do
+      ["", "multiple", "foo"].each do |value|
+        lambda {
+          SchemaEvolutionManager::MigrationFile::AttributeValue.new("transaction", value)
+        }.should raise_error(RuntimeError, "Attribute[transaction] - Invalid value[#{value}]. Must be one of: single none")
+      end
+    end
+
+  end
+
   it "for a valid file" do
     test_repo_with_script do |path|
       SchemaEvolutionManager::MigrationFile.new(path).path.should == path
@@ -27,12 +59,35 @@ describe SchemaEvolutionManager::MigrationFile do
     }.should raise_error(RuntimeError, "File[scripts/foo.sql] does not exist")
   end
 
+  def verify_transaction_value(values, value)
+    values.size.should == 1
+    values.first.attribute.name.should == "transaction"
+    values.first.value.should == value
+  end
+
   it "default attributes" do
+    test_repo_with_script do |path|
+      verify_transaction_value(SchemaEvolutionManager::MigrationFile.new(path).attribute_values, "single")
+    end
+  end
+
+  it "attribute transaction explicitly enabled" do
     command = <<-eos
-# ey
+# attribute.transaction=single
+select 1
     eos
     test_repo_with_script(:sql_command => command) do |path|
-      SchemaEvolutionManager::MigrationFile.new(path).attributes.should == [SchemaEvolutionManager::MigrationFile::Attribute::IN_TRANSACTION]
+      verify_transaction_value(SchemaEvolutionManager::MigrationFile.new(path).attribute_values, "single")
+    end
+  end
+
+  it "attribute transaction explicitly enabled" do
+    command = <<-eos
+# attribute.transaction=none
+select 1
+    eos
+    test_repo_with_script(:sql_command => command) do |path|
+      verify_transaction_value(SchemaEvolutionManager::MigrationFile.new(path).attribute_values, "none")
     end
   end
 
