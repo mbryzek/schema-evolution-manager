be26e29688b2edae50b84cdafee984bb2cf9a558
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-31 14:46:05 -0500

    autocommit: Update version to 0.9.23

--- lib/schema-evolution-manager/sem_version.rb
+++ lib/schema-evolution-manager/sem_version.rb
@@ -2,7 +2,7 @@ module SchemaEvolutionManager
 
   module SemVersion
 
-    VERSION = '0.9.22' # Automatically updated by util/create-release.rb
+    VERSION = '0.9.23' # Automatically updated by util/create-release.rb
 
   end
 
