e53058e4e39e09fadc3e63330572c2ac6fbc4c08
Gregor Heine <gregor@flow.io>
2018-05-25 13:51:30 +0100

    Add homebrew install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# derived from install script in https://github.com/sstephenson/bats
+set -e
+
+resolve_link() {
+  $(type -p greadlink readlink | head -1) "$1"
+}
+
+abs_dirname() {
+  local cwd="$(pwd)"
+  local path="$1"
+
+  while [ -n "$path" ]; do
+    cd "${path%/*}"
+    local name="${path##*/}"
+    path="$(resolve_link "$name" || true)"
+  done
+
+  pwd
+  cd "$cwd"
+}
+
+PREFIX="$1"
+if [ -z "$1" ]; then
+  { echo "usage: $0 <prefix>"
+    echo "  e.g. $0 /usr/local"
+  } >&2
+  exit 1
+fi
+
+SEM_ROOT="$(abs_dirname "$0")"
+mkdir -p "$PREFIX"/{bin,src}
+cp -R "$SEM_ROOT"/bin/* "$PREFIX"/bin
+cp -R "$SEM_ROOT"/lib/* "$PREFIX"/lib
+
+eval "./util/update-library-path.rb $PREFIX/bin/sem-config ../lib/schema-evolution-manager.rb $PREFIX/lib/schema-evolution-manager.rb"
+
+echo "Installed schema-evolution-manager scripts to $PREFIX/bin"
+
