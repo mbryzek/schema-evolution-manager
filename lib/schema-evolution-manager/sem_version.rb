49f3736685e99a4d2c763fd998c406fc4bad4fea
Gregor Heine <gregor@flow.io>
2018-05-31 08:49:19 -0400

    autocommit: Update version to 0.9.40

--- lib/schema-evolution-manager/sem_version.rb
+++ lib/schema-evolution-manager/sem_version.rb
@@ -2,7 +2,7 @@ module SchemaEvolutionManager
 
   module SemVersion
 
-    VERSION = '0.9.39' # Automatically updated by util/create-release.rb
+    VERSION = '0.9.40' # Automatically updated by util/create-release.rb
 
   end
 
