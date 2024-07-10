6fa8bf1dd94db742bdb8049f4619b7965be288de
Michael Bryzek <mike@flow.io>
2024-07-10 08:54:04 -0400

    Add option to prompt user when applying multiple scripts (#76)

--- lib/schema-evolution-manager/apply_util.rb
+++ lib/schema-evolution-manager/apply_util.rb
@@ -8,6 +8,11 @@ module SchemaEvolutionManager
         @dry_run = true
       end
 
+      @non_interactive = opts.delete(:non_interactive)
+      if @non_interactive.nil?
+        @non_interactive = false
+      end
+
       @db = Preconditions.assert_class(db, Db)
       @scripts = Scripts.new(@db, Scripts::SCRIPTS)
     end
@@ -16,14 +21,32 @@ module SchemaEvolutionManager
       @dry_run
     end
 
+    def non_interactive?
+      @non_interactive
+    end
+
     # Applies scripts in order, returning number of scripts applied
     def apply!(dir)
       Preconditions.check_state(File.directory?(dir),
                                 "Dir[%s] does not exist" % dir)
 
-      count = 0
+      pending_scripts = []
       @scripts.each_pending(dir) do |filename, path|
-        count += 1
+        pending_scripts << [filename, path]
+      end
+
+      if pending_scripts.size > 1 && !dry_run? && !non_interactive?
+        puts "Please confirm that you would like to apply all (#{pending_scripts.size}) of the pending scripts:"
+        pending_scripts.each do |filename, path|
+          puts "  #{filename}"
+        end
+        continue = SchemaEvolutionManager::Ask.for_boolean("Continue?")
+        if !continue
+          return 0
+        end
+      end
+
+      pending_scripts.each do |filename, path|
         if @dry_run
           puts "[DRY RUN] Applying #{filename}"
           puts path
@@ -35,9 +58,8 @@ module SchemaEvolutionManager
           puts " Done"
         end
       end
-      count
+      pending_scripts.size
     end
 
   end
-
 end
