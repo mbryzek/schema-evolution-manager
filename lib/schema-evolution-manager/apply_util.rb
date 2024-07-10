d0f900d4db6e30799500fef4bb26ed52b7f1cbb2
Michael Bryzek <mbryzek@alum.mit.edu>
2024-07-10 08:53:26 -0400

    wip

--- lib/schema-evolution-manager/apply_util.rb
+++ lib/schema-evolution-manager/apply_util.rb
@@ -8,12 +8,21 @@ module SchemaEvolutionManager
         @dry_run = true
       end
 
+      @non_interactive = opts.delete(:non_interactive)
+      if @non_interactive.nil?
+        @non_interactive = false
+      end
+
       @db = Preconditions.assert_class(db, Db)
       @scripts = Scripts.new(@db, Scripts::SCRIPTS)
     end
 
     def dry_run?
       @dry_run
+    end
+
+    def non_interactive?
+      @non_interactive
     end
 
     # Applies scripts in order, returning number of scripts applied
@@ -26,7 +35,7 @@ module SchemaEvolutionManager
         pending_scripts << [filename, path]
       end
 
-      if pending_scripts.size > 1 && !@dry_run
+      if pending_scripts.size > 1 && !dry_run? && !non_interactive?
         puts "Please confirm that you would like to apply all (#{pending_scripts.size}) of the pending scripts:"
         pending_scripts.each do |filename, path|
           puts "  #{filename}"
@@ -53,5 +62,4 @@ module SchemaEvolutionManager
     end
 
   end
-
 end
